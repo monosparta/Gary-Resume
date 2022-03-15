bd20b77ed4b5847c09eaf9094feed30df35a0b91
unknown <garyopen1876@gmail.com>
2022-03-15 11:26:55 +0800

    first organize

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import App from './page/Resume';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <Router>
+        <Routes>
+          <Route path="/" element={<App/>}/>
+        </Routes>
+      </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );

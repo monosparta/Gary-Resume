8cd456b7198c4abd5aa7930e9219b0b6f618475f
unknown <garyopen1876@gmail.com>
2022-03-29 02:02:34 +0800

    header finish

--- src/components/MyselfCard.js
+++ src/components/MyselfCard.js
@@ -1,33 +1,50 @@
 import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import HeaderCard from  '../img/header_card_img.jpg'
+
+
+const Style = {
+    card:{
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+    },
+    avatar: {
+        height: 150,
+        width: 150,
+        marginTop:-10,
+    },
+    
+}
 
 export default function MediaCard() {
-  return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component="img"
-        height="140"
-        image="/static/images/cards/contemplative-reptile.jpg"
-        alt="green iguana"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
-  );
+
+    return (
+        <Card style={Style.card}>
+        <CardMedia
+            component="img"
+            height="140"
+            image={HeaderCard}
+            alt="header_card"
+        />
+        <Avatar 
+            sx={Style.avatar} 
+            src={'https://i.pravatar.cc/300'} 
+            alt="me"
+        />
+        <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+                鈕臻昱
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+                一個不是在製造bug，就是在製造更多bugs的工程師。
+            </Typography>
+        </CardContent>
+        </Card>
+    );
 }
